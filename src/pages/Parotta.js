d7ec96fee7c9b9c664207fe6a0f48c733e0ac6bd
Vellaisamy-24 <vellaikarthick24@gmail.com>
2023-10-02 14:48:49 +0530

    push

--- src/pages/Parotta.js
+++ src/pages/Parotta.js
@@ -66,18 +66,18 @@ const Parotta = () => {
     ])
   return (
     <><div className='fixed bg-red-200 text-center w-screen top-0 left-0 right-0 text-red-500 sm:text-[50px] '>parotta</div>
-    <div className='bg-backgroundColor w-screen gap-4 mt-[90px] sm:grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4   '>
+    <div className='bg-backgroundColor w-screen gap-4 mt-[90px] sm:grid grid-cols-2  md:grid-cols-2 lg:grid-cols-3 lg:gap-8  2xl:grid-cols-4  '>
     {
       variety.map((food)=>
       (
         <div key={food.id}>
              <div className='mb-[100px]  md:gap-8 sm2:mb-[50px] sm:w-[400px] grid grid-cols lg-gap-8 sm1:border sm1:rounded-[30px] sm1:border-[5px] sm1:p-[5px] sm1:pt-[10px] lg:border lg:rounded-[30px] lg:p-[5px]  '>
-              <div className=''><img className="sm1:rounded-[10px] sm:rounded-[15px] sm1:w-screen sm2:h-auto sm2:object-cover sm:w-[300px] sm:gap-2 sm:h-[250px] lg:w-screen" src={food.image} alt=""/></div>
+              <div className=''><img className="sm1:rounded-[10px] sm:rounded-[15px] md:w-screen sm1:w-screen sm2:h-auto sm2:object-cover sm:w-screen sm:gap-2 sm:h-[250px] lg:w-screen" src={food.image} alt=""/></div>
      
         <div className='flex'>
           <div className=' pt-[1px] sm2:text-[25px] sm2:font-bold not-italic  mt-[5px] ml-[10px] lg:text-[27px] font-poppins text-black-500 sm:text-[25px] sm2:text-[30px] lg:-mt-[20px]'>{food.name}</div>
             <div className='mt-[9px] sm2:mt-[15px] not-italic font-montserrat lg:text-[25px]   sm:text-[20px] sm2:text-[25px] sm:ml-[30px] sm1:ml-[50px] lg:ml-[80px] lg:-mt-[14px] '>cost:</div>
-            <div className='ml-[5px] sm2:mt-[15px] mt-[9px] sm:text-[20px] sm2:text-[25px] text-red-300 lg:mt-[13px]  lg:mt-[19px] font-bold lg:-mt-[10px] '>&#8377;{food.cost}</div>
+            <div className='ml-[5px] sm2:mt-[15px] mt-[9px] sm:text-[20px] sm2:text-[25px] text-red-300    font-bold lg:-mt-[10px] '>&#8377;{food.cost}</div>
             <div>
         </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,13 @@ module.exports = {
         'sm2':'375px',
         'sm3':'390px',
         'sm4':'415px',
-        'sm': '640px',
+        'sm': '812px',
         // => @media (min-width: 640px) { ... }
   
         'md': '768px',
         // => @media (min-width: 768px) { ... }
   
-        'lg': '1024px',
+        'lg': '1192px',
         // => @media (min-width: 1024px) { ... }
   
         'xl': '1280px',
@@ -26,22 +26,9 @@ module.exports = {
         // => @media (min-width: 1536px) { ... }
       },
       colors: {
-        'custom-green': '#1EC089',
-        'custom-orange': '#FF7865',
-        'custom-back':'#FAFAFA',
-        'custom-gray': '#F0F0F0',
-        'linear-green-2': 'var(--Linear-Green-2, #1EC089)',
-        'gray-3': 'var(--Gray-3, #828282)',
-        'custom-gray-2': 'var(--Grey, #6A778B)',
-        'mountcolor':'#33D69F',
-        'dlxcolor':'#6A778B',
-        'breakcolor':'#FF7865',
-        'packtextcolor':'#FF7865',
-        'packplacecolor':'#6B7280',
-        'bookingtext':'#6A778B',
-        'bookingticket':'#FF7865',
+    
         'backgroundColor':"#fafafa",
-        'snowcolor':'#FF7865;',
+       
         
         
       },
