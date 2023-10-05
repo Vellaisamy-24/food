5dc2c999b3ffbe7436b3c21e93dddd98a6d20074
Vellaisamy-24 <vellaikarthick24@gmail.com>
2023-10-05 08:07:33 +0530

    addeddd

--- src/Login.js
+++ src/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
 
   return (
     <>
-    <div className=' 2xl:p-[15px] 2xl:mt-[250px] 2xl:w-[600px] h-auto  mx-auto my-auto mt-[100px] border border-black border-[1px] shadow-lg p-[10px] w-[300px] '>
+    <div className=' 2xl:p-[15px] 2xl:mt-[200px] 2xl:w-[600px] sm:mt-[150px] h-auto  mx-auto my-auto mt-[100px] border border-black border-[1px] shadow-lg p-[10px] w-[300px] '>
         <h1 className=' lg:text-[50px] text-[30px] text-center font-semibold font-ibm-plex-sans'>Login</h1>
     <form onSubmit={handleSubmit}>
     <div className='my-[15px] '>
@@ -25,12 +25,12 @@ const Login = () => {
     </div>
     <div  className='my-[15px]'>
         <div  className=' my-[10px]'> <label className='lg:text-[30px] font-montserrat text-[20px]'>Email</label></div>
-       <div> <input className="lg:h-[40px] rounded-[5px] " type="email"  placeholder='email' required onChange={(e)=>setUserMail(e.target.value)} /></div>
+       <div> <input className="lg:h-[40px] rounded-[5px] " type="email" required  placeholder='email'  onChange={(e)=>setUserMail(e.target.value)} /></div>
        
     </div>
     <div  className='my-[15px]'>
         <div  className=' my-[10px]'> <label className='lg:text-[30px] font-montserrat text-[20px]'>Password</label></div>
-       <div>  <input className=" lg:h-[40px] rounded-[5px]" type="password"  placeholder="password" required onChange={(e)=>setUserPassword(e.target.value)}/></div>
+       <div>  <input className=" lg:h-[40px] rounded-[5px]" type="password" required  placeholder="password"  onChange={(e)=>setUserPassword(e.target.value)}/></div>
       
     </div>
     <div  className='my-[15px] text-center '>
--- src/Order.js
+++ src/Order.js
@@ -1,17 +1,58 @@
 import React from 'react'
+import {useState,useContext} from "react"
 import {useParams} from "react-router-dom"
+import { LoginContext } from './App'
 const Order = () => {
+    const [display,setDisplay]=useState(false)
+    const {userName}=useContext(LoginContext)
     const {name,cost,quantity,totalprice}=useParams()
-  return (
-    <div className=' p-[10px] md:p-[20px] m md:ml-[900px] md:mt-[300px] border  md:w-[300px]'>
-        <div className='font-bold md:text-[40px] m-[10px] md:p-[15px]'> Bill Details</div>
-        <form>
-<div>Food Name:{name}</div>
-<div>Food Cost:{cost}</div>
-<div>{quantity}</div>
-<div>{totalprice}</div>
+    function handleSubmit(e)
+    {
+        e.preventDefault()
+    }
+    function place()
+    {
+      alert("Ordered successfull")
 
-            <input type="text" placeholder='Enter the adddress to be delivered' />
+    }
+  return (
+    <div className='bg-backgroundColor lg:w-[500px] p-[10px] md:p-[20px] m md:ml-[900px] md:mt-[300px] border  md:w-[300px]'>
+        <div className='font-bold md:text-[40px] m-[10px] md:p-[15px] text-orange-400'> Bill Details</div>
+        <form onSubmit={handleSubmit}>
+
+<div className='m-[10px]'>
+    <span className='font-bold font-montserrat md:text-[20px]'>Food Name:</span>
+    <span className='ml-[5px]'>{name}</span>
+    </div>
+<div className='m-[10px]'>
+    <span className='font-bold font-montserrat md:text-[20px]'>Food Cost:</span>
+    <span>{cost}</span>
+    </div>
+<div className='m-[10px]'>
+    <span className='font-bold font-montserrat md:text-[20px]'>Quantity:</span>
+    <span>{quantity}</span>
+    </div>
+    <div className='m-[10px]'>
+        <span className='font-bold font-montserrat md:text-[20px]'>User:</span>
+        <span>{userName}</span>
+        </div>
+    <div className='m-[10px]'><span className='font-bold font-montserrat md:text-[20px]'>TotalPrice:</span>
+    <span>{totalprice}</span></div>
+
+<div className='m-[10px]'>
+
+</div>
+<div className='m-[10px]'><span className='font-bold font-montserrat md:text-[20px] '>Address</span>
+
+            <span><input type="text" required placeholder='Enter the adddress to be delivered' /></span>
+            </div>
+            <div className='m-[10px]'>
+                <span className='font-bold font-montserrat md:text-[20px]'>Mobile.No</span>
+                <span><input type="number" required placeholder='number' /></span>
+            </div>
+            <div className='text-center mt-[30px] '>    <button className=' font-semibold p-[10px] text-[25px] shadow-lg rounded-[6px] hover:text-bold hover:text-orange-700 hover:bg-white font-ibm-plex-sans bg-violet-400 text-white' type="submit" onClick={place}>Confirm Order</button></div>
+        
+           
             </form>
     </div>
     
