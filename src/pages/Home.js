a3ff13855faf7a3a5afba3394752f487bd5878b9
Vellaisamy-24 <vellaikarthick24@gmail.com>
2023-10-06 20:00:14 +0530

    pls

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,34 +15,34 @@ const Home = () => {
   return (
     <>
 
- <div className='bg-gray-500  '>
+ <div className='bg-red-white '>
    
   <div className=' bg-gray-800 sm1:text-[40px] font-exquisite font-bold shadow-md border border-black border-[2px]  fixed  text-white w-screen top-0 left-0 right-0'>Zomato</div>
  
-    <div className='text-black fixed top-0 right-0 '><button  className='text-[40px]' onClick={handleDisplay}>Profile</button></div>
+    <div className='text-black fixed top-0 right-0 '><button  className='text-[40px] text-white' onClick={handleDisplay}>Profile</button></div>
     <div className='flex justify-center  items-center w-auto shadow-lg  '>
     <div className='mt-[100px] text-[30px] sm:text-[50px] text-center font-semibold font-ibm-plex-sans bg-violet-400 text-white text-center  w-[400px] sm:font-[40px]'>Menu</div>
     </div>
     
     <div className='grid grid-col sm1:mb-[50px]  text-textcolor h-screen mt-[50px] text-center text-orange-900 sm1:text-[30px]  w-screen text-[20px]'>
      
-    <div >
-      {display &&<div > <div className='text-black text-[20px] fixed right-0 mr-[130px]  ' >User:{userName}</div ><div className='text-black text-[20px] fixed right-0 mt-[40px] '>MailId:{userMail}</div></div>
+    <div className=''>
+      {display &&<div > <div className='text-black text-[20px]  fixed right-0 mr-[55px] sm:text-[30px]  sm:mr-[200px]  ' >User:{userName}</div ><div className='text-black text-[20px] fixed right-0 mt-[40px] sm:text-[30px]    '>MailId:{userMail}</div></div>
       }
       </div>
      
       
-        <div className=' mt-[5px] pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'><button className=' sm:mb-[30px] sm:shadow-2xl shadow-lg shadow-lg border md:border-[5px] w-[150px] h-[100px] '> <Link to="/parotta">Parotta</Link></button></div>
+        <div className=' mt-[5px] pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans '><button className=' sm:mb-[30px] lg:mb-[10px] shadow-lg border hover:border hover:border-[2px]  w-[150px] h-[100px] hover:bg-red-400 hover:text-white hover:shadow-2xl transition-transform hover:scale-105  hover:rounded-[7px]'> <Link to="/parotta">Parotta</Link></button></div>
         
-        <div className='pb-[5px] text-black font-semibold font-ibm-plex-sans'>
-        <button className='border sm:shadow-2xl shadow-lg w-[150px] h-[100px] '> <Link to="/dhosai">Dhosai</Link></button>
+        <div className='pb-[5px] text-orange-400  font-semibold font-ibm-plex-sans'>
+        <button className='border shadow-lg w-[150px] h-[100px] hover:bg-red-400 hover:text-white hover:shadow-2xl  lg:mb-[10px] transition-transform hover:scale-105 hover:rounded-[7px] '> <Link to="/dhosai">Dhosai</Link></button>
 </div>
-        <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'> <button className=' sm:mb-[30px]  border sm:shadow-2xl shadow-lg w-[150px] h-[100px] '> <Link to="/idiyappam">Idiyappam</Link></button></div>
-       <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'> <button className='border sm:shadow-2xl shadow-lg w-[150px] h-[100px] '> <Link to="/briyani">Briyani</Link></button></div>
+        <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'> <button className=' sm:mb-[30px]  border shadow-lg w-[150px] h-[100px] hover:bg-red-400 hover:text-white hover:rounded-[7px] hover:shadow-2xl lg:mb-[10px] transition-transform hover:scale-105 '> <Link to="/idiyappam">Idiyappam</Link></button></div>
+       <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'> <button className='border w-[150px] h-[100px] shadow-lg  hover:bg-red-400 hover:text-white hover:shadow-2xl lg:mb-[10px]  transition-transform hover:scale-105'> <Link to="/briyani">Briyani</Link></button></div>
        <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'>
-        <button className='sm:mb-[30px] border sm:shadow-2xl shadow-lg w-[150px] h-[100px] '> <Link to="/pongal">Pongal</Link></button></div>
-   <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'>  <button className='sm:mb-[30px] border sm:shadow-2xl shadow-lg w-[150px] h-[100px] '> <Link to="/vadai">Vadai</Link></button></div>
-      <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'><button className=' sm:mb-[30px] border sm:shadow-2xl shadow-lg w-[150px] h-[100px] '> <Link to="/idly">Idly</Link></button></div>
+        <button className='sm:mb-[30px] border   hover:bg-red-400 hover:text-white shadow-lg w-[150px] h-[100px]  lg:mb-[10px] transition-transform hover:scale-105 hover:rounded-[7px]'> <Link to="/pongal">Pongal</Link></button></div>
+   <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'>  <button className='sm:mb-[30px] border shadow-lg w-[150px] h-[100px] hover:bg-red-400 hover:text-white hover:shadow-2xl lg:mb-[10px] transition-transform  hover:rounded-[7px] hover:scale-105 '> <Link to="/vadai">Vadai</Link></button></div>
+      <div className='pb-[5px] text-orange-400 font-semibold font-ibm-plex-sans'><button className=' sm:mb-[30px] border  shadow-lg w-[150px] h-[100px] hover:bg-red-400 hover:text-white hover:shadow-2xl lg:mb-[10px] transition-transform hover:rounded-[7px]  hover:scale-105 '> <Link to="/idly">Idly</Link></button></div>
     
      
      
