fbfd4ece5b9311a91b7d55a06380d6d57ffffb93
Vellaisamy-24 <vellaikarthick24@gmail.com>
2023-10-06 22:47:32 +0530

    add

--- src/Price.js
+++ src/Price.js
@@ -28,7 +28,7 @@ const Price = () => {
          <div className='m-[20px] mb-[30px]  text-[20px] lg:ml-[360px] 2xl:ml-[380px] '><button className='hover:bg-red-600 border-[1px] border-black shadow-2xl  border p-[10px] rounded-[10px] font-exquisite font-semibold text-white bg-red-400' type="submit" onClick={cal}>Calculate Price</button></div>
          <div className='shadow-md rounded-[10px] p-[10px] 2xl:ml-[380px] sm:w-[300px] w-[200px] lg:w-[300px] text-[25px] font-light border-[1px] border-black  bg-yellow-300 sm:text-[30px] lg:text-[35px] m-[20px] lg:text-[40px] lg:ml-[360px] font-exquisite'><span className='text-black'>Total Price:</span><span className='text'>&#8377;{totalPrice}</span></div>
       </div>
-      <div className="text-center text-[30px]  "><button className='border  hover:bg-violet-500 sm1:p-[10px] lg:p-[15px] 2xl:w-[200px] shadow-lg rounded rounded-[9px] bg-violet-600 font-semibold font-poppins text-white'><Link to={`/placeOrder/${name}/${cost}/${quantity}/${totalPrice}`}>place oder </Link></button></div>
+      <div className="text-center text-[30px]  "><button className='border  hover:bg-violet-500 sm1:p-[10px] lg:p-[15px] 2xl:w-[200px] shadow-lg rounded rounded-[9px] bg-violet-600 font-semibold font-poppins text-white'><Link to={`/placeOrder/${name}/${cost}/${quantity}/${totalPrice}`}>place order </Link></button></div>
       </div>
         
 
